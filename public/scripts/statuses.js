71e0ac6931f7ce39fd239f16d8e20c736115bf62
Scott Neel <scott@neel.net>
2015-12-20 09:08:22 -0800

    * wire up statuses

--- public/scripts/statuses.js
+++ public/scripts/statuses.js
@@ -1,6 +1,58 @@
 'use strict';
 
 angular.module('statuses', [])
+.service('status', function ($interval, $timeout, $http) {
+  var rooms = {};
+  var loader;
+
+  console.log('statuses');
+
+  var errorResponse = function (room) {
+
+    return function (response) {
+      console.log('Error getting devices for room %s: %s', room, response);
+    };
+
+  };
+
+  var parseRoom = function (room) {
+
+    return function (response) {
+      rooms[room] = response.data;
+    };
+
+  };
+
+  var getRoom = function (room) {
+
+    $http({ url: '/rooms/' + room + '/devices' }).then(parseRoom(room), errorResponse(room));
+
+  };
+
+  var load = function () {
+    Object.keys(rooms).forEach(getRoom);
+  };
+
+  $interval(load, 10000);
+
+  return {
+    add_room: function (room) {
+
+      if (rooms[room] === undefined) {
+        rooms[room] = [];
+      }
+
+      if (loader !== undefined) {
+        $timeout.cancel(loader);
+      }
+
+      loader = $timeout(load, 500);
+    },
+    get: function (room) {
+      return rooms[room] || [];
+    }
+  };
+})
 .directive('statuses', function () {
 
   return {
@@ -24,12 +76,36 @@ angular.module('statuses', [])
     scope: {
       room: '@',
       icon: '@',
-      state: '@'
+      state: '@',
+      interval: '@'
     },
-    controller: function () {
+    controller: function ($scope, $interval, status) {
+      status.add_room($scope.room);
 
+      $scope.interval = $scope.interval || 2;
+      $scope.state = $scope.state || '_on';
+      $scope.alerting = 0;
+
+      var parseRoom = function () {
+        var data = status.get($scope.room) || [];
+
+        var alert = false,
+          alerting = 0;
+
+        data.forEach(function (room) {
+          if (room[$scope.state] === true) {
+            alert = true;
+            alerting += 1;
+          }
+        });
+
+        $scope.alert = alert;
+        $scope.alerting = alerting;
+      };
+
+      $interval(parseRoom, (1000));
     },
-    template: '<div class="{{icon}}"></div>',
+    template: '<div class="status {{icon}}"><span class="badge" ng-class="{alert: alert}">{{alerting}}</span></div>',
     replace: true,
   };
 
