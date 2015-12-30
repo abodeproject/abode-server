dc6c6a699ca57fcc24fda4648c8a507828c8920d
Scott Neel <scott@neel.net>
2015-12-29 16:27:34 -0800

    * initial display work

---
+++ providers/display/index.js
@@ -0,0 +1,208 @@
+'use strict';
+
+var abode,
+  routes,
+  config,
+  q = require('q'),
+  fs = require('fs'),
+  path = require('path'),
+  exec = require('child_process').exec;
+
+var logger = require('log4js'),
+  log = logger.getLogger('display');
+
+var Display = function () {
+  var defer = q.defer();
+  abode = require('../../abode');
+  routes = require('./routes');
+
+  abode.web.server.use('/api/display', routes);
+
+  config = Display.config || {};
+  config.interval = config.interval || 5;
+  config.enabled = (config.enabled === false) ? false : true;
+
+  if (config.enabled === true) {
+    log.info('Starting Display');
+
+    Display.load().then(function () {
+      Display.poller = setInterval(Display.load, 1000 * config.interval);
+    }, function (err) {
+      log.error('Display loader failed, not starting: ', err);
+    });
+
+    defer.resolve();
+  } else {
+    log.warn('Not starting Display.  Not enabled');
+    defer.resolve();
+  }
+
+  return defer.promise;
+};
+
+Display.load = function () {
+  var defer = q.defer();
+  var base = '/sys/class/backlight';
+
+  var failLoad = function (msg) {
+    defer.reject(msg);
+  };
+
+  var get_monitor_power = function () {
+    var mon_defer = q.defer();
+
+    var process = function (error, stdout, stderr) {
+      if (error) {
+        log.error('Could not determine power:', stderr);
+        mon_defer.resolve();
+        return;
+      }
+
+      Display.on = (stdout.indexOf('Monitor is On') !== -1);
+      log.info('Monitor power:', Display.on);
+      mon_defer.resolve();
+    };
+
+    exec('xset -q', process);
+
+    return mon_defer.promise;
+  };
+
+  var get_maxbacklight = function () {
+    var bl_defer = q.defer();
+    var bl_path = path.join(base, Display.display, 'max_brightness');
+
+    fs.readFile(bl_path, 'utf8', function (err, data) {
+      if (err) {
+        bl_defer.reject(err);
+        return;
+      }
+
+      Display.max_brightness = parseInt(data, 10);
+      log.info('Max brightness:', Display.max_brightness);
+      bl_defer.resolve(data);
+    });
+
+    return bl_defer.promise;
+  };
+
+  var get_currentbacklight = function () {
+    var bl_defer = q.defer();
+    var bl_path = path.join(base, Display.display, 'brightness');
+
+    fs.readFile(bl_path, 'utf8', function (err, data) {
+      if (err) {
+        bl_defer.reject(err);
+        return;
+      }
+
+      Display.brightness = parseInt(data, 10);
+      log.info('Current brightness:', Display.brightness);
+      bl_defer.resolve(data);
+    });
+
+    return bl_defer.promise;
+  };
+
+  var parseDisplay = function (display) {
+
+    Display.display = display;
+    log.info('Parsing display:', Display.display);
+
+    get_currentbacklight()
+    .then(get_maxbacklight, failLoad)
+    .then(get_monitor_power, failLoad);
+  };
+
+  var findDisplay = function (displays) {
+
+    if (displays.length === 0) {
+      failLoad('Failed to find a display');
+    } else if (displays.length === 1) {
+      parseDisplay(displays[0]);
+    } else {
+      if (config.display) {
+        if (displays.indexOf(config.display) !== -1) {
+          parseDisplay(displays.indexOf(config.display));
+        } else {
+          failLoad('Failed to find configured display: ', config.display);
+        }
+      } else {
+        log.warn('Multiple displays found, using first: ', displays[0]);
+        parseDisplay(displays[0]);
+      }
+    }
+
+  };
+
+  log.info('Loading display properties');
+  fs.readdir(base, function (err, displays) {
+    if (!err) {
+      findDisplay(displays);
+    } else {
+      failLoad('No backlight class found');
+    }
+  });
+
+  return defer.promise;
+};
+
+Display.set_brightness = function (brightness) {
+  var defer = q.defer();
+
+  brightness = parseInt(brightness, 10);
+
+  if (isNaN(brightness) || brightness > Display.max_brightness) {
+    defer.reject({'status': 'failed', 'message': 'Invalid brightness specified'});
+    return defer.promise;
+  }
+
+  var b_handler = function (err, stdout, stderr) {
+    if (err) {
+      defer.resolve({'status': 'failed', 'message': stdout, 'error': stderr});
+      return;
+    }
+
+    defer.resolve({'status': 'success'});
+  };
+
+  exec('set_brightness ' + Display.display + ' ' + brightness, b_handler);
+
+  return defer.promise;
+};
+
+Display.on = function () {
+  var defer = q.defer();
+
+  var b_handler = function (err, stdout, stderr) {
+    if (err) {
+      defer.resolve({'status': 'failed', 'message': stderr});
+      return;
+    }
+
+    defer.resolve({'status': 'success'});
+  };
+
+  exec('xset -dpms', b_handler);
+
+  return defer.promise;
+};
+
+Display.off = function () {
+  var defer = q.defer();
+
+  var b_handler = function (err, stdout, stderr) {
+    if (err) {
+      defer.resolve({'status': 'failed', 'message': stderr});
+      return;
+    }
+
+    defer.resolve({'status': 'success'});
+  };
+
+  exec('xset +dpms && xset dpms force off', b_handler);
+
+  return defer.promise;
+};
+
+module.exports = Display;
---
+++ providers/display/routes.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var display = require('../display'),
+  express = require('express'),
+  router = express.Router();
+
+router.get('/', function (req, res) {
+
+  res.send({
+    'name': display.display,
+    'power': display.on,
+    'brightness': display.brightness,
+    'max_brightness': display.max_brightness,
+  });
+
+});
+
+router.get('/brightness', function (req, res) {
+
+  res.send({'brightness': display.brightness});
+
+});
+
+router.post('/brightness/:value', function (req, res) {
+
+  display.set_brightness(req.params.value).then(function (response) {
+    res.status(200).send(response);
+  }, function (err) {
+    res.status(400).send(err);
+  });
+
+});
+
+router.get('/max_brightness', function (req, res) {
+
+  res.send({'max_brightness': display.max_brightness});
+
+});
+
+router.get('/power', function (req, res) {
+
+  res.send({'power': display.on});
+
+});
+
+router.post('/on', function (req, res) {
+
+  display.on().then(function (response) {
+    res.status(200).send(response);
+  }, function (err) {
+    res.status(400).send(err);
+  });
+
+});
+
+router.post('/off', function (req, res) {
+
+  display.off().then(function (response) {
+    res.status(200).send(response);
+  }, function (err) {
+    res.status(400).send(err);
+  });
+
+});
+
+module.exports = router;
