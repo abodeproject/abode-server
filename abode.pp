eb453ad07731af39b536079e2cbe0bb4a6b2164e
Scott Neel <scott@neel.net>
2016-01-06 20:38:52 -0800

    * add more packages

--- abode.pp
+++ abode.pp
@@ -7,6 +7,15 @@ $packages = [
   'mongodb-server',
   'rpi-update',
   'apache2',
+  'omxplayer',
+  'gconf-service',
+  'libgtk2.0-0',
+  'libgnome-keyring0',
+  'libnspr4',
+  'libnspr4-0d',
+  'libnss3',
+  'libxss1',
+  'xdg-utils',
 ]
 
 $service = [
