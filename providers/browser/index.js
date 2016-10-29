692e7c175be618d27df7ca77db9b93e66ae285f0
Scott Neel <scott@neel.net>
2016-10-29 15:49:47 -0700

    * add stub browser provider

---
+++ providers/browser/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var abode,
+  routes,
+  config,
+  q = require('q');
+
+var logger = require('log4js'),
+  log = logger.getLogger('browser');
+
+var Browser = function () {
+  var defer = q.defer();
+  abode = require('../../abode');
+  //routes = require('./routes');
+
+  //abode.web.server.use('/api/browsers', routes);
+
+  abode.config.browser = abode.config.browser || {};
+  abode.config.browser.enabled = (abode.config.browser.enabled === false) ? false : true;
+
+  config = abode.config.browser || {};
+  log.info('Browser Loaded');
+  defer.resolve();
+
+  return defer.promise;
+};
+
+module.exports = Browser;
