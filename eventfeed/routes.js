0f4618a568a5ec798c3e15eb5789fa1d753075ac
Scott Neel <scott@neel.net>
2016-12-11 13:31:36 -0800

    * remove time emit

--- eventfeed/routes.js
+++ eventfeed/routes.js
@@ -89,14 +89,6 @@ router.get('/:key', function (req, res) {
           res.write('data:' + JSON.stringify(event.event) + '\n\n'); // Note the extra newline
         });
 
-        if (abode.providers.time) {
-          var timestamp = new Date(),
-            timestamp = timestamp.getTime();
-
-          res.write('id: ' + timestamp + '\n');
-          res.write('data:' + JSON.stringify({'type': 'TIME_CHANGE', 'name': abode.providers.time.time, 'object': abode.providers.time.toJSON()}) + '\n\n');
-        }
-
         abode.eventfeed.clients.push(res);
         abode.events.emit('CLIENT_CONNECTED', {'type': 'eventfeed', 'client': res});
 
@@ -121,14 +113,6 @@ router.get('/:key', function (req, res) {
           'Connection': 'keep-alive'
       });
       res.write('\n');
-
-      if (abode.providers.time) {
-        var timestamp = new Date(),
-          timestamp = timestamp.getTime();
-          
-        res.write('id: ' + timestamp + '\n');
-        res.write('data:' + JSON.stringify({'type': 'TIME_CHANGE', 'name': abode.providers.time.time, 'object': abode.providers.time.toJSON()}) + '\n\n'); // Note the extra newline
-      }
 
       abode.eventfeed.clients.push(res);
       abode.events.emit('CLIENT_CONNECTED', {'type': 'eventfeed', 'client': res});
